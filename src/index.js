b33f18db3eb2136d4e22cfb21dfbc620ac840148
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-07-21 12:05:55 -0400

    Add some content to side bar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './style.css';
 import './to-do-list.png';
+
+const BarCheck = () => {
+    if (window.innerWidth >= 900) {
+        // console.log('hi');
+    }
+};
+
+BarCheck();
+
+// window.addEventListener('resize', BarCheck);
