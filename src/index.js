9f8440bade0c10f78c44975913e6ad032c06dfa1
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-07-23 12:20:05 -0400

    Make sidebar change with screen size

--- src/index.js
+++ src/index.js
@@ -2,13 +2,34 @@ import './style.css';
 import './add.svg';
 import './github-icon.svg';
 
-// const BarCheck = () => {
-//     if (window.innerWidth >= 900) {
-//         // console.log('hi');
-//     } else {
-//     }
-// };
+const sidebar = document.querySelector('.sidebar');
+const sidettl = document.querySelector('.proj-title');
 
-// BarCheck();
+const BarCheck = () => {
+    if (window.innerWidth >= 900) {
+        if (sidebar.hasAttribute('style')) {
+            sidebar.removeAttribute('style');
+        }
 
-// window.addEventListener('resize', BarCheck);
+        if (sidettl.hasAttribute('style')) {
+            sidettl.removeAttribute('style');
+        }
+
+        if (!sidebar.classList.contains('sb-active')) {
+            sidebar.classList.toggle('sb-active');
+        }
+    } else {
+        sidettl.style['justify-content'] = 'center';
+        sidebar.style.width = '100%';
+        sidebar.style.textAlign = 'center';
+    }
+};
+
+BarCheck();
+
+window.addEventListener('resize', BarCheck);
+
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', () => {
+    sidebar.classList.toggle('sb-active');
+});
