717e5c94105cb3a49c53533ea9d55b4732f19b75
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-07-24 11:50:07 -0400

    Make menu respond with sidebar visibility
    The sidebar will now automatically close up whenever the user's screen
    width is detected to be less than 900 pixels. This makes it so mobile
    users don't have the menu automatically opened up whenever they view
    the page.

--- src/index.js
+++ src/index.js
@@ -2,34 +2,48 @@ import './style.css';
 import './add.svg';
 import './github-icon.svg';
 
-const sidebar = document.querySelector('.sidebar');
-const sidettl = document.querySelector('.proj-title');
+(() => {
+    const sidebar = document.querySelector('.sidebar');
+    const sidettl = document.querySelector('.proj-title');
+    const menu = document.querySelector('.menu');
 
-const BarCheck = () => {
-    if (window.innerWidth >= 900) {
-        if (sidebar.hasAttribute('style')) {
-            sidebar.removeAttribute('style');
+    const MenuAnimation = () => {
+        menu.classList.toggle('opened');
+        menu.setAttribute('aria-expanded', menu.classList.contains('opened'));
+    };
+
+    const BarCheck = () => {
+        if (window.innerWidth >= 900) {
+            if (sidebar.hasAttribute('style')) {
+                sidebar.removeAttribute('style');
+            }
+
+            if (sidettl.hasAttribute('style')) {
+                sidettl.removeAttribute('style');
+            }
+
+            if (!sidebar.classList.contains('sb-active')) {
+                sidebar.classList.toggle('sb-active');
+                MenuAnimation();
+            }
+        } else {
+            if (sidebar.classList.contains('sb-active')) {
+                sidebar.classList.toggle('sb-active');
+                MenuAnimation();
+            }
+
+            sidettl.style['justify-content'] = 'center';
+            sidebar.style.width = '100%';
+            sidebar.style.textAlign = 'center';
+            sidebar.style['font-size'] = '2em';
+            sidebar.style.gap = '20px';
         }
+    };
 
-        if (sidettl.hasAttribute('style')) {
-            sidettl.removeAttribute('style');
-        }
+    BarCheck();
+    window.addEventListener('resize', BarCheck);
 
-        if (!sidebar.classList.contains('sb-active')) {
-            sidebar.classList.toggle('sb-active');
-        }
-    } else {
-        sidettl.style['justify-content'] = 'center';
-        sidebar.style.width = '100%';
-        sidebar.style.textAlign = 'center';
-    }
-};
-
-BarCheck();
-
-window.addEventListener('resize', BarCheck);
-
-const menu = document.querySelector('.menu');
-menu.addEventListener('click', () => {
-    sidebar.classList.toggle('sb-active');
-});
+    menu.addEventListener('click', () => {
+        sidebar.classList.toggle('sb-active');
+    });
+})();
