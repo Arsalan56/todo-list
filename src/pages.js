2e3234739bb22ca548cf59916b163890451089a5
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-08-23 16:19:46 -0400

    Finish code for today page
    The filtering for the today page is finished. When the user clicks the
    today button, the tasks that aren't today are hidden.

--- src/pages.js
+++ src/pages.js
@@ -13,20 +13,25 @@ export default function Page(list) {
     todayPage.addEventListener('click', () => {
         HideSide();
         const today = format(new Date(), 'MM/dd/yyyy');
-
         list.forEach((item) => {
-            const date = item.due.split('-');
-
-            if (
-                format(
-                    new Date(`${date[1]}-${date[2]}-${date[0]}`),
-                    'MM/dd/yyyy'
-                ) === today
-            ) {
-                console.log('today');
+            const div = document.querySelectorAll('.item')[list.indexOf(item)];
+            if (!item.due) {
+                div.style.visibility = 'hidden';
+                div.style.position = 'absolute';
             } else {
-                //
+                const date = item.due.split('-');
+
+                if (
+                    format(
+                        new Date(`${date[1]}-${date[2]}-${date[0]}`),
+                        'MM/dd/yyyy'
+                    ) !== today
+                ) {
+                    div.style.visibility = 'hidden';
+                    div.style.position = 'absolute';
+                }
             }
         });
+        document.querySelector('.header').textContent = 'Today';
     });
 }
